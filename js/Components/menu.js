bacf1b3d093a5d55c3d5d96a0805ea1dff6fa6e9
gardner589 <ctgardner89@gmail.com>
2019-11-15 13:09:01 -0500

    submenus implemented

---
+++ js/Components/menu.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', function(){
+
+  let megamenuButtons = document.querySelectorAll('.accordion-button.nav-link');
+
+  [...megamenuButtons].map( elm => {
+
+    let submenu = elm.nextElementSibling;
+
+    elm.addEventListener('click', (evt) => {
+      let elmExpanded = elm.getAttribute('aria-expanded');
+      let submenuHidden = submenu.getAttribute('aria-hidden');
+      elm.classList.toggle('submenu-item-active');
+
+      if (elmExpanded == 'false') {
+        elm.setAttribute('aria-expanded', 'true')
+        submenu.setAttribute('aria-hidden','false')
+      } else {
+        elm.setAttribute('aria-expanded', 'false')
+        submenu.setAttribute('aria-hidden','true')
+      }
+    })
+
+  })
+
+  let submenuItems = document.querySelectorAll('.nav-submenu > .has-submenu');
+
+  [...submenuItems].map( elm => {
+    let submenu_child = elm.querySelector('.nav-submenu-submenu');
+
+    elm.addEventListener('click', (evt) => {
+      elm.classList.toggle('submenu-item-active');
+    })
+  })
+
+})
--- js/farmers.js
+++ js/farmers.js
@@ -2,6 +2,7 @@
 import './Components/accordion.js'
 import './Components/checkbox.js'
 import './Components/radio_buttons.js'
+import './Components/menu.js'
 import Stepper from './Components/Steppers/Vertical/Stepper.js'
 
 window.Stepper = Stepper;
