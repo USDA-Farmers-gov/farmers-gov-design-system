36e236cde1ecba8fbb88a86e2ebefcba4d9231d1
Matt Calvert <mcalvert@metrostarsystems.com>
2019-11-16 13:40:35 -0600

    styles and js updates

--- js/farmers.js
+++ js/farmers.js
@@ -3,6 +3,7 @@ import './Components/accordion.js'
 import './Components/checkbox.js'
 import './Components/radio_buttons.js'
 import './Components/menu.js'
+import './Components/mobile_menu.js'
 import Stepper from './Components/Steppers/Vertical/Stepper.js'
 
 window.Stepper = Stepper;
