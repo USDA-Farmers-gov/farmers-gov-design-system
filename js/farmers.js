5f880a1a35ac59a361259c76052b4e6f437a3760
Matt Calvert <mcalvert@metrostarsystems.com>
2019-11-16 13:50:06 -0600

    Revert "styles and js updates"
    This reverts commit 36e236cde1ecba8fbb88a86e2ebefcba4d9231d1.

--- js/farmers.js
+++ js/farmers.js
@@ -3,7 +3,6 @@ import './Components/accordion.js'
 import './Components/checkbox.js'
 import './Components/radio_buttons.js'
 import './Components/menu.js'
-import './Components/mobile_menu.js'
 import Stepper from './Components/Steppers/Vertical/Stepper.js'
 
 window.Stepper = Stepper;
