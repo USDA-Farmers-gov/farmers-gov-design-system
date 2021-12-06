d0ffe1ef2cd2c434ea3b9ad376d25c926b35759c
Matt Calvert <mcalvert@metrostarsystems.com>
2021-12-06 16:03:40 -0500

    mcalvert/vue-stepper
    refactoring code

--- js/stepper/components/App.vue
+++ js/stepper/components/App.vue
@@ -4,15 +4,14 @@
       <h2>{{ data.stepper_header }}</h2>
       <p>{{ data.stepper_description }}</p>
       <hr />
-
       <div
-        :class="inputRowClasses(step, stepIndex)"
+        :class="rowClasses(step, stepIndex)"
         v-for="(step, stepIndex) in data.questions"
         v-if="stepIsVisible(stepIndex) || stepIndex === 0"
       >
         <div class="question-number">
           <div class="number-container">
-            <div class="number">{{ stepIndex + 1 }}</div>
+            <div class="number">{{ stepNumber(stepIndex) }}</div>
           </div>
         </div>
         <div class="question-and-answer">
@@ -69,67 +68,54 @@ export default {
     this.resetStepper();
   },
   methods: {
-    inputRowClasses(step, stepIndex) {
+    rowClasses(step, stepIndex) {
       let classes = ["input-row"];
-      let answered = this.visibleSteps.filter(
-        (row) => row.stepIndex === stepIndex + 1
-      );
-      if (!!answered.length) classes.push("answered");
+      if (!!this.visibleSteps.filter((row) => row.stepIndex > stepIndex).length)
+        classes.push("answered");
 
       return classes.join(" ");
+    },
+    stepNumber(stepIndex) {
+      return (
+        this.visibleSteps.findIndex((row) => row.stepIndex === stepIndex) + 1
+      );
     },
     resetStepper() {
       this.visibleSteps = [{ stepIndex: 0 }];
       this.results = [];
+    },
+    stepIsVisible(stepIndex) {
+      return this.visibleSteps.filter((row) => row.stepIndex === stepIndex)
+        .length;
+    },
+    optionIsChecked(value, stepIndex) {
+      return Boolean(
+        this.visibleSteps.findIndex(
+          (row) => row.stepIndex === stepIndex && row.answer === value
+        ) > -1
+      );
     },
     processAnswer(option, stepIndex) {
       // remove questions if their index is higher than the one currently selected.
       this.visibleSteps = this.visibleSteps.filter(
         (row) => row.stepIndex <= stepIndex
       );
+      this.results = [];
 
       if (!!option.value) this.setAnswer(stepIndex, option.value);
-
-      if (!!option.go_to) {
-        this.results = [];
-        this.setAnswer(option.go_to - 1);
-      }
-
+      if (!!option.go_to) this.setAnswer(option.go_to - 1);
       if (!!option.result) {
-        const results_index = this.results.findIndex(
-          (row) => row.stepIndex === stepIndex
+        this.results = this.results.filter(
+          (row) => row.stepIndex !== stepIndex
         );
-
-        if (results_index > -1) {
-          this.results = this.results.filter((idx) => idx !== results_index);
-        }
-
         this.results.push({ stepIndex: stepIndex, result: option.result });
       }
     },
-    stepIsVisible(stepIndex) {
-      return this.visibleSteps.filter((row) => row.stepIndex === stepIndex)
-        .length
-        ? true
-        : false;
-    },
-    optionIsChecked(value, stepIndex) {
-      const answerCheck = this.visibleSteps.findIndex(
-        (row) => row.stepIndex === stepIndex && row.answer === value
-      );
-
-      return answerCheck > -1 ? true : false;
-    },
     setAnswer(index, value) {
       this.results = [];
-      const existing_index = this.visibleSteps.findIndex(
-        (row) => row.stepIndex === index
+      this.visibleSteps = this.visibleSteps.filter(
+        (row) => row.stepIndex !== index
       );
-      if (existing_index > -1) {
-        this.visibleSteps = this.visibleSteps.filter(
-          (idx) => idx !== existing_index
-        );
-      }
       this.visibleSteps.push({ stepIndex: index, answer: value });
     },
     getResult(index) {
