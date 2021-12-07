abbc1ca4f3c7d67526413987fac9e9b0bc000ceb
Matt Calvert <mcalvert@metrostarsystems.com>
2021-12-07 08:59:20 -0500

    mcalvert/vue-stepper
    Removed load event listener when intializing the stepper.

--- js/stepper/farmers-stepper.js
+++ js/stepper/farmers-stepper.js
@@ -4,12 +4,10 @@ import App from "./components/App.vue";
 require("./mixins").initialize(Vue);
 
 export default function FGStepper(data) {
-  window.addEventListener("load", (event) => {
-    const el = document.getElementById(data.element_id);
-    var app = new Vue({
-      el: el,
-      render: (h) => h(App, { props: { data: data } }),
-    });
+  const el = document.getElementById(data.element_id);
+  var app = new Vue({
+    el: el,
+    render: (h) => h(App, { props: { data: data } }),
   });
 }
 
