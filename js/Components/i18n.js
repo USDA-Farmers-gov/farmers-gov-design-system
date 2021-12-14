aa38c476e26bf327cd3339ece839b61acb32412d
Matt Calvert <mcalvert@metrostarsystems.com>
2021-12-14 16:23:15 -0500

    mcalvert/i18n
    Updated i18n script

--- js/Components/i18n.js
+++ js/Components/i18n.js
@@ -2,7 +2,8 @@ export function translate(json) {
   let langJson = json["en"];
   const htmlLang = document.querySelector("html").getAttribute("lang");
 
-  if (htmlLang !== "en") langJson = json[key];
+  if (!!htmlLang && htmlLang !== "en") langJson = json[htmlLang];
+
   if (htmlLang === "en") {
     Object.keys(json).forEach(function (key) {
       if (window.location.href.indexOf(`/${key}/`) > -1) langJson = json[key];
