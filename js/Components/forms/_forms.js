6e3c592a136bb33689c92f9156aae1af7045ce11
Matt Calvert <mcalvert@metrostarsystems.com>
2022-01-26 12:52:58 -0500

    mcalvert/FG-4604.Screen.Reader.Inputs.508
    Style and accessiblity updates

--- js/Components/forms/_forms.js
+++ js/Components/forms/_forms.js
@@ -1,9 +1,6 @@
 import * as checkboxes_and_radios from "./checkboxes_and_radios";
-import * as forms_accessibility from "./forms_accessibility";
 
 window.addEventListener("load", (event) => {
   checkboxes_and_radios.checkboxCards();
   checkboxes_and_radios.radioButtonCards();
-  forms_accessibility.checkboxKeyboard();
-  forms_accessibility.radioButtonKeyboard();
 });
