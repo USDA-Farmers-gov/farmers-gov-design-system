e6fae0bf973b459c299c73072c5dde25d5d65c9d
Matt Calvert <>
2023-08-09 15:53:28 -0400

    FG-5707.Static.Column.Table.Fix ## Fixed issue where static mobile column was not being activated on page load

--- js/Components/mobile_tables.js
+++ js/Components/mobile_tables.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   setTimeout(() => {
     saveTableWidths();
     processMobileTables();
-  }, "2000");
+  }, "1500");
 });
 
 window.addEventListener("resize", function () {
@@ -25,30 +25,15 @@ function saveTableWidths() {
 
 function processMobileTables() {
   const tables = document.querySelectorAll("table");
-  const staticColClass = "show-mobile-static-column";
   if (!!tables.length) {
-    const widths = JSON.parse(sessionStorage.getItem("ds_table_widths"));
-    const bodyWidth = document.body.getBoundingClientRect().width;
-
     tables.forEach((table, idx) => {
       if (
-        !!table.closest(".mobile-static-column-container") &&
-        widths[idx].tableWidth > bodyWidth &&
-        table.getBoundingClientRect().width > bodyWidth
+        !!table.classList &&
+        table.classList.contains("mobile-static-column")
       ) {
-        table
-          .closest(".mobile-static-column-container")
-          .classList.add(staticColClass);
+        processStaticColTable(table, idx);
+        setupStaticColContainer(table);
       }
-
-      if (
-        !!table.closest(".mobile-static-column-container") &&
-        widths[idx].tableWidth < bodyWidth &&
-        table.getBoundingClientRect().width < bodyWidth
-      )
-        table
-          .closest(".mobile-static-column-container")
-          .classList.remove(staticColClass);
 
       const headers = table.querySelectorAll("thead th");
       const dataRows = table.querySelectorAll("tr");
@@ -59,22 +44,7 @@ function processMobileTables() {
         dataRows,
         headersInData
       );
-      if (
-        table.classList.contains("mobile-static-column") &&
-        !table.closest(".mobile-static-column-container")
-      ) {
-        const tableStaticCol = table.cloneNode(true);
-        const newDiv = document.createElement("div");
 
-        tableStaticCol.classList.add("show-on-mobile");
-
-        newDiv.classList.add("mobile-static-column-container");
-        newDiv.appendChild(tableStaticCol);
-
-        table.after(newDiv);
-        table.remove();
-        return;
-      }
       if (!simpleTable) {
         if (table.classList.contains("mobile-static-column")) return;
         table.classList.add("show-on-mobile");
@@ -90,6 +60,56 @@ function processMobileTables() {
   }
 }
 
+function processStaticColTable(table, idx) {
+  const staticColClass = "show-mobile-static-column";
+  const widths = JSON.parse(sessionStorage.getItem("ds_table_widths"));
+  const bodyWidth = document.body.getBoundingClientRect().width;
+
+  table.classList.add("full-width");
+
+  // if the table goes outside of the body container, add class
+  // to activate static column
+  if (
+    widths[idx].tableWidth > bodyWidth &&
+    table.getBoundingClientRect().width > bodyWidth
+  )
+    table.classList.add(staticColClass);
+
+  // if the table is inside of the body container, remove class
+  // for static column
+  if (
+    widths[idx].tableWidth < bodyWidth &&
+    table.getBoundingClientRect().width < bodyWidth
+  )
+    table.classList.remove(staticColClass);
+
+  // if there's a large margin between the table and right
+  // margin of the body, remove the static column class
+  if (
+    bodyWidth - table.getBoundingClientRect().width > 50 &&
+    !!table.classList.contains("show-mobile-static-column")
+  )
+    table.classList.remove(staticColClass);
+}
+
+function setupStaticColContainer(table) {
+  if (
+    table.classList.contains("mobile-static-column") &&
+    !table.closest(".mobile-static-column-container")
+  ) {
+    const tableStaticCol = table.cloneNode(true);
+    const newDiv = document.createElement("div");
+
+    tableStaticCol.classList.add("show-on-mobile");
+
+    newDiv.classList.add("mobile-static-column-container");
+    newDiv.appendChild(tableStaticCol);
+
+    table.after(newDiv);
+    table.remove();
+  }
+}
+
 function setupTableForMobile(table) {
   if (table.classList.contains("mobile-static-column")) return;
   table.classList.add("simple-table");
@@ -101,7 +121,7 @@ function setupTableForMobile(table) {
       const headerCells = row.querySelectorAll("th");
       const tableCells = row.querySelectorAll("td");
 
-      if (tableCells.length <= 2) table.classList.remove("simple-table");
+      // if (tableCells.length <= 2) table.classList.remove("simple-table");
 
       if (!!headerCells.length) {
         headerData = [];
@@ -122,7 +142,8 @@ function setupTableForMobile(table) {
 function checkIfSimpleTable(table, headers, dataRows, headersInData) {
   let integer = 0;
 
-  if (!!table && table.classList.contains("ui-datepicker-calendar")) integer++;
+  if (!!table.classList && table.classList.contains("ui-datepicker-calendar"))
+    integer++;
 
   headers.forEach(function (header) {
     if (header.hasAttribute("colspan") || header.hasAttribute("rowspan"))
